229df0f49c408db16454889aebb3de1228654cc5
Karla Espinosa <karlae2703@gmail.com>
2026-08-02 00:00:47 -0600

    MDC: Adding cursor-pointer to Go back button

--- components/products/GoBackButton.tsx
+++ components/products/GoBackButton.tsx
@@ -8,7 +8,7 @@ export const GoBackButton = () => {
   return (
     <button
       onClick={() => router.back()}
-      className="inline-flex items-center gap-2 text-neutral-300 transition hover:text-lime-400"
+      className="inline-flex items-center gap-2 text-neutral-300 transition hover:text-lime-400 cursor-pointer"
     >
       <span aria-hidden="true">←</span>
       Go back
